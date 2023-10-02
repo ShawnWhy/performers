6b3b7ca32975d9abd513b13f050f3a4fa86df918
Shawn Yu <shawnyudesign@gmail.com>
2023-10-02 19:51:25 -0400

    sdsdSDS

---
+++ script.js
@@ -0,0 +1,25 @@
+var treeColor = "cadetBlue";
+var branchFractal = 6;
+var degrees = 100;
+var mouseLeft = 500;
+
+ var red = Math.floor(Math.random() * 250 + 100);
+ var green = Math.floor(Math.random() * 100 + 60);
+ var blue = Math.floor(Math.random() * 50 + 20);
+ 
+      let redRand = Math.floor(Math.random() * 100 - 40);
+      let greenRand = Math.floor(Math.random() * 100 - 40);
+      let blueRand = Math.floor(Math.random() * 100 - 40);
+
+      appleColor =
+        "rgb(" +
+        (red + redRand) +
+        "," +
+        (green + greenRand) +
+        "," +
+        (blue + blueRand) +
+        ")";
+
+
+ 
+
